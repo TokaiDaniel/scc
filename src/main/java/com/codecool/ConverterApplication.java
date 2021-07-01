45d3b889163917c7401073447916e76ecabf2f28
Daniel Tokai <tokaidaniel@gmail.com>
2021-07-01 16:28:57 +0200

    No input file handled

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -2,6 +2,8 @@ package com.codecool;
 
 public class ConverterApplication {
     public static void main(String[] args) {
-
+        if (args.length == 0) {
+            System.out.println("No input file defined");
+        }
     }
 }
