9c6eee89432661153bf4f3701b4fca046aa57bb8
Daniel Tokai <tokaidaniel@gmail.com>
2021-07-01 16:56:06 +0200

    Handle command line arguments

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -1,9 +1,27 @@
 package com.codecool;
 
+import java.io.File;
+
 public class ConverterApplication {
     public static void main(String[] args) {
+        File file;
+        OutputFormat outputFormat;
+
         if (args.length == 0) {
             System.out.println("No input file defined");
+        }
+        else if (args.length == 1){
+            outputFormat = OutputFormat.TABLE;
+            file = new File(args[1]);
+        }
+        else if (args.length == 2){
+            if (args[0].equals("JSON")){
+                outputFormat = OutputFormat.JSON;
+            }
+            else if(args[0].equals("XML")){
+                outputFormat = OutputFormat.XML;
+            }
+            file = new File(args[1]);
         }
     }
 }
---
+++ src/main/java/com/codecool/OutputFormat.java
@@ -0,0 +1,5 @@
+package com.codecool;
+
+public enum OutputFormat {
+    JSON, XML, TABLE
+}
